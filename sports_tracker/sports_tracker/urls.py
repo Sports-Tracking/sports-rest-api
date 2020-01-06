bf233a5dbe9fde0849df095e76faf9884928e35b
farooq azam khan <farooq1.khan@ryerson.ca>
2020-01-06 14:51:09 -0500

    added templating

--- sports_tracker/sports_tracker/urls.py
+++ sports_tracker/sports_tracker/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import TemplateView
+
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name="index"),
     path('soccer/', include('soccer.urls')),
     path('admin/', admin.site.urls),
     path('hockey/', include('hockey.urls'))
