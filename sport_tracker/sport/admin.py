1c41c8587fd59394828be019cec6f1e6ea82ebd5
farooq azam khan <farooq1.khan@ryerson.ca>
2020-01-07 22:11:29 -0500

    revert

--- sport_tracker/sport/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# generic, root
-from sport.models import League
-# Register your models here.
-admin.site.register(League)
--- sport_tracker/team/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from team.models import Team
-
-
-class TeamSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Team
-        fields = ['name', 'location', 'arena_name', 'wins', 'ties', 'losses']
